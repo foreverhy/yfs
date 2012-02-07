17005827a3fd5f3a3cd207e20f561e29c8d0996d
Yandong Mao <ydmao@mit.edu>
2012-02-07 13:00:26 -0500

    Updating lab1. Details about the update are availabe on Piazza, if necessary.

--- rpc/rpc.cc
+++ rpc/rpc.cc
@@ -643,11 +643,14 @@ rpcs::dispatch(djob_t *j)
 }
 
 // rpcs::dispatch calls this when an RPC request arrives.
+//
 // checks to see if an RPC with xid from clt_nonce has already been received.
 // if not, remembers the request in reply_window_.
-// deletes remembered requests (and free the corresponding reply_t::buf) less than
-// xid_rep, which is the first xid for which the client says it is still
-// waiting for a reply.
+//
+// deletes remembered requests with XIDs less than xid_rep, which is
+// the first xid for which the client says it is still waiting for a reply.
+// frees the reply_t::buf of each such request.
+//
 // returns one of:
 //   NEW: never seen this xid before.
 //   INPROGRESS: seen this xid, and still processing it.
