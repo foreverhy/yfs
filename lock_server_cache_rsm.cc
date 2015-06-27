7af6f32bc61d0ef231df2025aba2823793e11eb6
Yao Han <verihy@gmail.com>
2015-06-27 16:46:12 +0800

    Marshall and unmarshall state for lock server

--- lock_server_cache_rsm.cc
+++ lock_server_cache_rsm.cc
@@ -8,6 +8,7 @@
 #include "lang/verify.h"
 #include "handle.h"
 #include "tprintf.h"
+#include "marshall.h"
 
 
 static void *
@@ -34,6 +35,7 @@ lock_server_cache_rsm::lock_server_cache_rsm(class rsm *_rsm)
   VERIFY (r == 0);
   r = pthread_create(&th, NULL, &retrythread, (void *) this);
   VERIFY (r == 0);
+  rsm->set_state_transfer(this);
 }
 
 void
@@ -166,14 +168,59 @@ lock_server_cache_rsm::release(lock_protocol::lockid_t lid, std::string id,
 std::string
 lock_server_cache_rsm::marshal_state()
 {
-  std::ostringstream ost;
-  std::string r;
-  return r;
+    //std::ostringstream ost;
+    //std::string r;
+    marshall rep;
+    using ull = unsigned long long;
+    std::lock_guard<std::mutex> m_(mtxtb);
+    rep << static_cast<ull>(locktb.size());
+    for (auto &lock : locktb) {
+        ull lid = lock.second->lid; 
+        int status = lock.second->status; 
+        std::string owner = lock.second->owner;
+        rep << lid << status << owner;
+        ull retrysize = lock.second->retry.size();
+        rep << retrysize;
+        for (size_t i = 0; i < retrysize; ++i) {
+            std::string retryer = lock.second->retry.front();
+            rep << retryer; 
+            lock.second->retry.pop();
+            lock.second->retry.push(retryer);
+        }
+        ull xid = lock.second->xid;
+        rep << xid;
+    }
+    return rep.str();
 }
 
 void
 lock_server_cache_rsm::unmarshal_state(std::string state)
 {
+    unmarshall rep(state);
+    using ull = unsigned long long;
+    std::lock_guard<std::mutex> m_(mtxtb);
+    locktb.clear();
+    ull tbsize;
+    rep >> tbsize;
+    for (size_t i = 0; i < tbsize; ++i) {
+        ull lid;
+        int status;
+        std::string owner;
+        ull retrysize;
+        rep >> lid >> status >> owner >> retrysize;
+        auto rlk = new server_lock(lid);
+        rlk->status = status;
+        rlk->owner = owner;
+        std::string retryer;  
+        for (size_t i = 0; i < retrysize; ++i) {
+            rep >> retryer;
+            rlk->retry.push(retryer);
+        }
+        ull xid;
+        rep >> xid;
+        rlk->xid = xid;
+        locktb[lid] = rlk;
+    }
 }
 
 lock_protocol::status
